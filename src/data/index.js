495d1f1330e0fffe7a9881df59194448febc6067
andrewflower-rs <arnaldomahone@hotmail.com>
2024-03-06 23:34:01 -0300

    minuto 46

---
+++ src/data/index.js
@@ -0,0 +1,7 @@
+const fs = require('fs')
+const path = require('path')
+module.exports = {
+saveData : (data , filenameJSON = "productsDataBase") =>{
+const pathJSON = path.join(__dirname , `./${filenameJSON}.json`)
+}
+}
